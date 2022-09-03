aaedf668566b75d7f596681f03ddb2b696942837
Ruslan <klimenkoruslan11@gmail.com>
2022-09-03 17:16:48 +0300

    app'

--- mysite/settings.py
+++ mysite/settings.py
@@ -165,9 +165,9 @@ if os.getcwd() == '/app':
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATIC_URL = '/static/'
-    STATICFILES_DIRS = ['static/images',
-                        'static/blog',
-                        'staticfiles']
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
