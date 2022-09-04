abe51fb7e470970a6531a50144be2ce15407d918
Ruslan <klimenkoruslan11@gmail.com>
2022-09-04 11:43:05 +0300

    upgrade images

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,7 +126,7 @@ LOGIN_URL = '/users/login'
 LOGIN_REDIRECT_URL = 'blog:post_index'
 LOGOUT_REDIRECT_URL = 'users:login'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
 
 # Social-Auth
 AUTHENTICATION_BACKENDS = (
